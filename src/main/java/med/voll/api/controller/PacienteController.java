58e686442290a0f5bc5290fa7ff442c0b025272f
chrisbmineiro <chrisbmineiro@gmail.com>
2025-01-03 23:55:27 -0300

    feat: adicionando validações as entidades com Validation Bean

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.dto.CadastrarPacienteDTO;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PacienteController {
 
     @PostMapping
-    public void cadastrarPaciente(@RequestBody CadastrarPacienteDTO dados){
+    public void cadastrarPaciente(@RequestBody @Valid CadastrarPacienteDTO dados){
         System.out.println(dados);
     }
 }
