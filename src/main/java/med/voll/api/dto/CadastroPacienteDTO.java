ed270271ff51f625edcaaa2139553093cae32cb3
chrisbmineiro <chrisbmineiro@gmail.com>
2025-01-08 00:30:11 -0300

    feat: tratando erros de exceptions e mensagens personalizadas

--- src/main/java/med/voll/api/dto/CadastroPacienteDTO.java
+++ src/main/java/med/voll/api/dto/CadastroPacienteDTO.java
@@ -7,17 +7,17 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record CadastroPacienteDTO(
-        @NotBlank
+        @NotBlank(message = "{nome.obrigatorio}")
         String nome,
-        @NotBlank
-        @Email
+        @NotBlank(message = "{email.obrigatorio}")
+        @Email(message = "{email.invalido}")
         String email,
-        @NotBlank
+        @NotBlank(message = "{telefone.obrigatorio")
         String telefone,
-        @NotBlank
-        @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}")
+        @NotBlank(message = "{cpf.obrigatorio}")
+        @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}", message = "{cpf.invalido}")
         String cpf,
-        @NotNull
+        @NotNull(message = "{endereco.obrigatorio}")
         @Valid
         EnderecoDTO endereco
 ) {
