17367d8e977d649784791bc526c19b20b9d8c1ce
chrisbmineiro <chrisbmineiro@gmail.com>
2025-01-08 21:34:51 -0300

    feat: criando tabela usuarios no banco de dados e adicionando class Usuario, interface UsuarioRepository e class AutenticacaoService

---
+++ src/main/java/med/voll/api/models/Usuario.java
@@ -0,0 +1,22 @@
+package med.voll.api.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+}
